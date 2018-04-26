ef276c614d144f875ede767b2fa7a38a7fcad86b
Onion <yangsongv587person@163.com>
2018-04-26 14:28:35 +0800

    图片浏览demo

---
+++ photoBookDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  photoBookDemo
+//
+//  Created by Onion on 2018/4/26.
+//  Copyright © 2018年 Onion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ photoBookDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Onion on 2018/4/25.
+//  Copyright © 2018年 Onion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
